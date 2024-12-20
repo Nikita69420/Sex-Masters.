e7fb044191cfdf0293ca3ac2e6e43a9e7692e0bf
Nikita69420 <Nikita69420@users.noreply.github.com>
2024-12-20 17:16:49 +0100

    Yes true

--- src/Components/2D.jsx
+++ src/Components/2D.jsx
@@ -7,7 +7,7 @@ function Card1() {
             <img className="card-image" src={profilePic} alt="profile picture"></img>
             <h2 className="card-title">Ambatukam</h2>
             <p className="card-text">Tools collector</p>
-            <a className='text' href='https://cdn.discordapp.com/attachments/1290660645695520808/1299488668339277884/Ambatukam.exe?ex=6724a2e1&is=67235161&hm=b565490c0290e3f8de3bbe03fc6d2a2c20648c97e6ae0c0c7ce3d0b98b295d72&'>Download</a>
+            <a className='text' href='https://cdn.discordapp.com/attachments/1290660645695520808/1299488668339277884/Ambatukam.exe?ex=67668de1&is=67653c61&hm=448c03fa13c4fee7f53393ddf5c88e8a6c5f0e7f6652aeb4407f8d1ddac152ee&'>Download</a>
         </div>
     );
 }
